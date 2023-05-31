b24643f1ac77175b53d42552774f52124dad0347
Debby <debby.schweinfest@systemli.org>
2023-05-31 14:45:14 +0200

    initial commmit

---
+++ src/main/java/GroupProject.java
@@ -0,0 +1,53 @@
+public class GroupProject {
+
+    public static void main (String[] args) {
+        //int sum = sumUp(1, 2);
+        //System.out.println(sum);
+        //System.out.println(returnSumAsString(3, 4));
+
+        //System.out.println(calculateFactorial(5));
+        System.out.println(otherWayToFaculty(5));
+
+    }
+
+    public static int sumUp(int a, int b){
+        int sum = a + b;
+        return sum;
+
+    }
+    public static String returnSumAsString(int c, int d){
+        int result = c + d;
+        return "Summe: " + result;
+    }
+
+    public static int calculateFactorial(int number){
+        int i;
+        int result = 1;
+        for (i = 1; i <= number; i++) {
+
+            result = result * i;
+
+        }
+        return result;
+    }
+
+    public static int otherWayToFaculty(int x){
+        int i;
+        int result = x;
+        for (i = x-1; i > 0; i--) {
+
+            result = result * i;
+            System.out.println(result);
+    }
+        return result;
+    }
+
+    public static int fac(int y){
+        int i;
+        while (i <= y) {
+
+        }
+    }
+
+}
+
